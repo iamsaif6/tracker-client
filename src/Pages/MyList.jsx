9deba90e78041a9804f4d028d0094ed64cb2d6f1
Saif Hasan <saifhassan399@gmail.com>
2024-04-29 13:46:16 +0600

    Table added for my spot list

--- src/Pages/MyList.jsx
+++ src/Pages/MyList.jsx
@@ -1,7 +1,9 @@
 import { useContext, useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { AuthContext } from '../Components/AuthProvider';
-import TouristSpot from '../Components/TouristSpot';
+import { FaEye, FaEdit } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const MyList = () => {
   const { user } = useContext(AuthContext);
@@ -21,10 +23,67 @@ const MyList = () => {
       </Helmet>
       <div>
         <h2 className="text-[30px] font-semibold text-center my-9">My Tour Spot List</h2>
-        <div className="grid grid-cols-3 gap-7 mb-[50px] mt-[30px]">
-          {spots.map(spot => (
-            <TouristSpot key={spot._id} spot={spot}></TouristSpot>
-          ))}
+        <div>
+          <div className="overflow-x-auto">
+            <table className="table">
+              {/* head */}
+              <thead>
+                <tr>
+                  <th>Spot Name</th>
+                  <th>Location</th>
+                  <th>Avg Cost</th>
+                  <th>Travel Time</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* row 1 */}
+                {spots.map(spot => (
+                  <tr key={spot._id}>
+                    <td>
+                      <div className="flex items-center gap-3">
+                        <div className="avatar">
+                          <div className="mask mask-squircle w-12 h-12">
+                            <img src={spot.image} alt="Avatar Tailwind CSS Component" />
+                          </div>
+                        </div>
+                        <div>
+                          <div className="font-bold">{spot.tourists_spot_name}</div>
+                          <div className="text-sm opacity-50">{spot.country_Name}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <td>
+                      {spot.location}
+                      <br />
+                      <span className="badge badge-ghost badge-sm">{spot.country_Name}</span>
+                    </td>
+                    <td>${spot.average_cost}</td>
+                    <td>
+                      <p>{spot.travel_time}</p>
+                    </td>
+                    <td className="text-[20px] flex items-center gap-3">
+                      <Link to={`/details/${spot._id}`}>
+                        <button title="View">
+                          <FaEye className="text-[22px] mt-[2px]" />
+                        </button>
+                      </Link>
+                      <Link>
+                        <button title="Edit">
+                          <FaEdit />
+                        </button>
+                      </Link>
+                      <Link>
+                        <button title="Delete">
+                          <MdDelete />
+                        </button>
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
